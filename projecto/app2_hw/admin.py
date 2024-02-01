67254e5f3712b1bd43f26ee7913515f0086e5ac2
maslaff <maslaff@yandex.ru>
2024-02-01 20:28:21 +0300

    hw5 Solved

--- projecto/app2_hw/admin.py
+++ projecto/app2_hw/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from app2_hw.models import *
 
 # Register your models here.
+
+
+@admin.action(description="Обнулить количества")
+def reset_quantity(modeladmin, request, queryset):
+    queryset.update(quantity=0)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    """Список продуктов."""
+
+    list_display = ["name", "quantity"]
+    list_filter = ["add_date", "price"]
+    search_fields = ["description"]
+    search_help_text = "Поиск по полю Описание продукта (description)"
+    actions = [reset_quantity]
+
+
+admin.site.register(User)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
